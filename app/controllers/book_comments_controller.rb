4d2f6ed984a7b5510804fe02106212a331d12f1e
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-03-03 07:45:02 +0000

    [Update] コメント非同期通信化（未完成）

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,15 +3,13 @@ class BookCommentsController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    @comment = BookComment.new(book_comment_params)
-    @comment.book_id = @book.id
-    @comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    comment = @book.book_comments.find(params[:id])
-    comment.destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
   end
 
   private
